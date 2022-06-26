30a7bfc500da27bb663eb15654a5b9f953dc22eb
Suryaansh Rai <suryaanshrai@gmail.com>
2022-06-26 13:21:21 +0530

    Added check feature

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, render_template, request
-import cv2 as cv
 
 # The rubik_package is based on an older version of python. The following lines avoid any errors arising from that. 
 import collections.abc
@@ -14,6 +13,9 @@ app = Flask(__name__)
 
 def apology(message):
     return render_template("apology.html", message=message)
+
+def apologyToHome(message):
+    return render_template("apologyToHome.html", message=message)
 
 def convert(x):
     cmoves = {
@@ -31,19 +33,15 @@ def index():
 @app.route("/inyellow", methods=["GET", "POST"])
 def inyellow():
     if request.method == "POST":
-        if not request.form.get("yellow") and not request.form.get("syellow"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("yellow"):
-            global yellow
-            yellow = cv.imread(request.form.get("yellow"))
-        else:
-            global syellow
-            syellow = request.form.get("syellow").strip()
-            if len(syellow) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in syellow:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("syellow"):
+            return apology("Input Required.")
+        global syellow
+        syellow = request.form.get("syellow").strip().upper()
+        if len(syellow) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in syellow:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/inblue")
     return render_template("inyellow.html")
 
@@ -51,19 +49,15 @@ def inyellow():
 @app.route("/inblue", methods=["GET", "POST"])
 def inblue():
     if request.method == "POST":
-        if not request.form.get("blue") and not request.form.get("sblue"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("blue"):
-            global blue
-            blue = cv.imread(request.form.get("blue"))
-        else:
-            global sblue
-            sblue = request.form.get("sblue").strip()
-            if len(sblue) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in sblue:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("sblue"):
+            return apology("Input Required.")
+        global sblue
+        sblue = request.form.get("sblue").strip().upper()
+        if len(sblue) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in sblue:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/inred")
     return render_template("inblue.html")
 
@@ -71,19 +65,15 @@ def inblue():
 @app.route("/inred", methods=["GET", "POST"])
 def inred():
     if request.method == "POST":
-        if not request.form.get("red") and not request.form.get("sred"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("red"):
-            global red
-            red = cv.imread(request.form.get("red"))
-        else:
-            global sred
-            sred = request.form.get("sred").strip()
-            if len(sred) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in sred:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("sred"):
+            return apology("Input Required.")
+        global sred
+        sred = request.form.get("sred").strip().upper()
+        if len(sred) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in sred:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/ingreen")
     return render_template("inred.html")
 
@@ -91,19 +81,15 @@ def inred():
 @app.route("/ingreen", methods=["GET", "POST"])
 def ingreen():
     if request.method == "POST":
-        if not request.form.get("green") and not request.form.get("sgreen"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("green"):
-            global green
-            green = cv.imread(request.form.get("green"))
-        else:
-            global sgreen
-            sgreen = request.form.get("sgreen").strip()
-            if len(sgreen) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in sgreen:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("sgreen"):
+            return apology("Input Required.")
+        global sgreen
+        sgreen = request.form.get("sgreen").strip().upper()
+        if len(sgreen) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in sgreen:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/inorange")
     return render_template("ingreen.html")
 
@@ -111,19 +97,15 @@ def ingreen():
 @app.route("/inorange", methods=["GET", "POST"])
 def inorange():
     if request.method == "POST":
-        if not request.form.get("orange") and not request.form.get("sorange"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("orange"):
-            global orange
-            orange = cv.imread(request.form.get("orange"))
-        else:
-            global sorange
-            sorange = request.form.get("sorange").strip()
-            if len(sorange) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in sorange:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("sorange"):
+            return apology("Input Required.")
+        global sorange
+        sorange = request.form.get("sorange").strip().upper()
+        if len(sorange) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in sorange:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/inwhite")
     return render_template("inorange.html")
 
@@ -131,19 +113,15 @@ def inorange():
 @app.route("/inwhite", methods=["GET", "POST"])
 def inwhite():
     if request.method == "POST":
-        if not request.form.get("white") and not request.form.get("swhite"):
-            return apology("Atleast one input is required out of image and text")
-        if request.form.get("white"):
-            global white
-            white = cv.imread(request.form.get("white"))
-        else:
-            global swhite
-            swhite = request.form.get("swhite").strip()
-            if len(swhite) != 9:
-                return apology("Not all colors were filled, try submitting again")
-            for i in swhite:
-                if i not in ['w','W','y','Y','r','R','o','O','b','B','g','G']:
-                    return apology("Some colors were not correct, try submitting again")
+        if not request.form.get("swhite"):
+            return apology("Input Required.")
+        global swhite
+        swhite = request.form.get("swhite").strip().upper()
+        if len(swhite) != 9:
+            return apology("Not all colors were filled, try submitting again")
+        for i in swhite:
+            if i not in ['W', 'Y', 'G', 'B', 'R', 'O']:
+                return apology("Some colors were not correct, try submitting again")
         return redirect("/solution")
     return render_template("inwhite.html")
 
@@ -151,9 +129,9 @@ def inwhite():
 def solution():
     try:
         cube = syellow + sblue + sred + sgreen + sorange + swhite
+        soln = utils.solve(cube, 'Kociemba')
     except:
-        return apology("Input Error. Try inputing the cube again.")
-    soln = utils.solve(cube, 'Kociemba')
+        return apologyToHome("Input Error. Try inputing the cube again.")
     solnString = ""
     for i in soln:
         solnString += str(convert(str(i)))
